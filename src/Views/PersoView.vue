d5aa778cedb6962ecddc2514ac8809cb4f9e7ca1
Alexandre <alexandre.prigent2910@gmail.com>
2025-04-07 17:10:09 +0200

    fix(emmit)fix emmits

--- src/Views/PersoView.vue
+++ src/Views/PersoView.vue
@@ -3,18 +3,24 @@ import { ref } from 'vue';
 import { ingredients } from '@/assets/structureEat.js';
 import CheckBox from '@/components/CheckBox.vue';
 const choices = ref(ingredients);
+const newMenu = ref({ viandes: [], feculents: [], legumes: [], dessert: [] });
+
+const recupMeal = (data, type) => {
+    newMenu.value.viandes.push(data);
+    console.log('recupMeal', newMenu.value);
+}
 </script>
 
 <template>
-    
+
     <h2>Viandes</h2>
-    <CheckBox :type="choices[0].viandes" class="flex gap-5" />
+    <CheckBox class="flex gap-5" :type="choices[0].viandes" @changeBox="recupMeal" />
     <h2>Feculents</h2>
     <CheckBox :type="choices[1].feculents" class="flex gap-5" />
     <h2>Legumes</h2>
     <CheckBox :type="choices[2].legumes" class="flex gap-5" />
     <h2>Produits laitiers</h2>
-    <CheckBox :type="choices[3].Dessert" class="flex gap-5"/>
+    <CheckBox :type="choices[3].Dessert" class="flex gap-5" />
 
 </template>
 
--- src/components/CheckBox.vue
+++ src/components/CheckBox.vue
@@ -1,25 +1,38 @@
 <script setup>
-defineProps({
+import { ref } from 'vue';
+const props = defineProps({
     type: Object,
 
 })
-import { ref } from 'vue';
+const emit = defineEmits(['changeBox']);
+function recupcheckMeal(index) {
+    console.log('checked', props.type[index]);
+    emit('changeBox', props.type[index]);
+
+}
+
+
+
 let checked = ref([]);
 
 </script>
 
 <template>
     <fieldset>
-        <div v-for="one, index in type" :key="index" :class="checked[index] ? 'backdrop-brightness-50' : ' '" class="flex flex-wrap backdrop-blur-md backdrop-brightness-100 backdrop-contrast-100 text-white items-center mb-4 flex-col gap-5 justify-center p-2 rounded-lg border-2 border-gray-300 
+        <div v-for="one, index in type" :key="index" :class="checked[index] ? 'backdrop-brightness-50' : ' '" class="flex flex-wrap backdrop-blur-md 
+        backdrop-brightness-100 backdrop-contrast-100 text-white items-center mb-4 flex-col gap-5 justify-center 
+        p-2 rounded-lg border-2 border-gray-300 
             transition duration-300 ease-in-out transform hover:scale-105 w-50">
             <legend></legend>
             <img src="../assets/img/Default.webp" alt="" class="w-20 h-20 rounded-lg object-cover" />
-            <p class="text-sm text-gray-600">{{ type[index].description }}</p>
-            <input type="checkbox" :id="type.label" :value="type.label" class="mr-2" v-model="checked[index]" />
-            <label :for="type.label" class="text-lg font-bold">{{ type[index].label }}</label>
+            <p class="text-sm text-gray-600">{{ one.description }}</p>
+            <input type="checkbox" :id="type.label" :value="type.label" class="mr-2" v-model="checked[index]"
+                @change="recupcheckMeal(index)" />
+            <label :for="type.label" class="text-lg font-bold">{{ one.label }}</label>
+
         </div>
     </fieldset>
-
+    {{ console.log(checked) }}
 </template>
 
 <style scoped></style>
